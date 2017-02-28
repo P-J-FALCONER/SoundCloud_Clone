cce6d8f234dbaef01d80a2458e3fbe2a097aaf50
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-02-28 10:39:28 -0600

    top charts page framed out

--- app_api/models/song.js
+++ app_api/models/song.js
@@ -5,6 +5,9 @@ var SongSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
+  name: {
+    type:String
+  },
   userLikes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
