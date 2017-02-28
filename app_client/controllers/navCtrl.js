bbf4994ed2631184b7c9d8203a07576199a24ec2
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-02-28 09:58:45 -0600

    changes made

--- app_client/controllers/navCtrl.js
+++ app_client/controllers/navCtrl.js
@@ -1,7 +1,9 @@
 angular.module('soundcloud')
   .controller('navCtrl', ['$scope', 'authFactory', '$location', function($scope, authFactory, $location){
     $scope.currentPath = $location.path();
-
+    $scope.isNavCollapsed = true;
+    $scope.isCollapsed = false;
+    $scope.isCollapsedHorizontal = false;
     authFactory.getCurrentUser().then(function(response){
       if('google' in response.data){
         $scope.user = response.data.google.username;
@@ -18,6 +20,7 @@ angular.module('soundcloud')
       authFactory.logout()
         .then(function(response){
           $scope.user = '';
+          $location.url('/')
         })
         .catch(function(response){
           console.log(response.data);
--- app_client/factories/contentFactory.js
+++ app_client/factories/contentFactory.js
@@ -1,6 +1,8 @@
 angular.module('soundcloud')
   .factory('contentFactory', ['$http', '$window', function($http, $window){
     return{
-
+      getCurrentUser: function(){
+        return $http.get('/api/user');
+      }
       }
   }])
