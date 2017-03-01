b62696144b9d1c87ce25e53204f713e78a2c9e0b
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-03-01 10:59:49 -0600

    blah

--- app_client/controllers/topChartCtrl.js
+++ app_client/controllers/topChartCtrl.js
@@ -2,7 +2,6 @@ angular.module('soundcloud')
   .controller('topChartCtrl', ['$scope','contentFactory', '$location', function($scope, contentFactory, $location){
     $scope.songs=[];
    contentFactory.getSongs().then(function(res){
-     console.log(res.data);
      $scope.songs = res.data;
    });
 
