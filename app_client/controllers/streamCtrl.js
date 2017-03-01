0c47d5f94aa95b0937e986c902d9e2f14e24a14c
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-03-01 10:16:39 -0600

    search for user on stream works

--- app_client/controllers/streamCtrl.js
+++ app_client/controllers/streamCtrl.js
@@ -2,6 +2,7 @@ angular.module('soundcloud')
   .controller('streamCtrl', ['$scope','contentFactory', '$location', function($scope, contentFactory, $location){
     $scope.users = [];
     contentFactory.getUsers().then(function(res){
+      console.log(res.data)
       $scope.users = res.data
     })
   }])
