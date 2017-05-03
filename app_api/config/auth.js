5ece312b9df235e245449f1bb7938d4275ebc4e9
Nick Phillips <phillipn101@gmail.com>
2017-05-03 14:06:04 -0500

    change callback url

--- app_api/config/auth.js
+++ app_api/config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
   'facebookAuth' : {
     'clientID'      : process.env.FB_CLIENT_ID, // your App ID
     'clientSecret'  : process.env.FB_CLIENT_SECRET, // your App Secret
-    'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
+    'callbackURL'   : 'http://54.172.134.183/auth/facebook/callback'
   },
   'googleAuth' : {
     'clientID'      : process.env.CLIENT_ID,
     'clientSecret'  : process.env.CLIENT_SECRET,
-    'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+    'callbackURL'   : 'http://54.172.134.183/auth/google/callback'
   }
 };
